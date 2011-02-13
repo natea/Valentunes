b14f043f55329b6b186e0b73c62cba5ebe7a0050
Nate Aune <natea@jazkarta.com>
2011-02-12 19:35:44 -0500

    First crack at the handlers for the card.

---
+++ vt/api/handlers.py
@@ -0,0 +1,23 @@
+from piston.handler import BaseHandler
+from piston.utils import rc
+from vt.valentunes.models import CardModel
+from vt.valentunes.forms import CardModelForm
+
+class CardHandler(BaseHandler):
+    allowed_methods = ('POST', 'GET', 'DELETE',)
+    
+    def create(self, request):
+        f = CardModelForm(request.POST)
+        if f.is_valid():
+            new_object = f.save()
+            return new_object
+        return rc.BAD_REQUEST
+        
+    def read(self, request, object_id):
+        card_object = CardModel.objects.get(id=object_id)
+        return card_object
+        
+    def delete(self, request, object_id):
+        card_object = CardModel.objects.get(id=object_id)
+        card_object.delete()
+        return rc.DELETED
