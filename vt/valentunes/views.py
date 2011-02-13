ae585055a70e8e621e039aaf323f7012ced5d325
Matt Katz <gitorious@morelightmorelight.com>
2011-02-13 08:12:56 -0500

    chooser added!

--- vt/valentunes/views.py
+++ vt/valentunes/views.py
@@ -1,8 +1,15 @@
 # Create your views here.
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from models import TrackModel, CardModel
 
 def index(request,template_name='index.html'):
     context = {}
+    return render_to_response(template_name, context,context_instance=RequestContext(request))
+
+def choose(request, template_name='choose.html'):
+    track_list = TrackModel.objects.filter(card='18')
+
+    context = {'track_list':track_list}
     return render_to_response(template_name, context,context_instance=RequestContext(request))
 
